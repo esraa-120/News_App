9bf20c767f4fe02204358c0a428677df0da2c2f7
Esraa <42310532.esraa@acu.edu.eg>
2026-09-17 07:00:59 +0300

    Refactor home module to implement MVVM architecture using Provider.
    - Implement `HomeViewModel` to centralize state management for categories, news sources, and article lists.
    - Migrate home-related files from `lib/modules/home/pages` to `lib/modules/home/views` and update directory structure.
    - Convert `HomePage` to a `StatelessWidget` and delegate UI logic to the view model.
    - Replace `FutureBuilder` in `SelectedCategoryView` with reactive Provider updates for handling asynchronous data fetching.
    - Update `AppRouter` to provide `HomeViewModel` to the home route using `ChangeNotifierProvider`.
    - Refactor category selection and tab switching to use view model methods instead of local widget state.

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/modules/home/pages/home_page.dart';
+import 'package:news_app/modules/home/view_model/home_view_model.dart';
+import 'package:news_app/modules/home/views/home_page.dart';
 import 'package:news_app/modules/splash/presentation/pages/splash_page.dart';
+import 'package:provider/provider.dart';
 
 import 'app_routes_name.dart';
 
@@ -15,7 +17,9 @@ class AppRouter {
         );
         case AppRoutesName.home:
         return MaterialPageRoute(
-          builder: (context) => const HomePage(),
+          builder: (context) => ChangeNotifierProvider<HomeViewModel>
+            (create: (context) => HomeViewModel(),
+              child: HomePage()),
         );
         default:
           return MaterialPageRoute(
--- lib/modules/home/views/home_page.dart
+++ lib/modules/home/views/home_page.dart
@@ -3,42 +3,29 @@ import 'package:news_app/core/l10n/app_localizations.dart';
 import 'package:news_app/core/theme/app_colors.dart';
 import 'package:news_app/gen/assets.gen.dart';
 import 'package:news_app/main.dart';
-import 'package:news_app/models/category_model.dart';
-import 'package:news_app/modules/home/pages/widgets/category_card_item.dart';
-import 'package:news_app/modules/home/pages/widgets/drawer_builder_view.dart';
-import 'package:news_app/modules/home/pages/widgets/selected_category_view.dart';
+import 'package:news_app/modules/home/views/widgets/category_card_item.dart';
+import 'package:news_app/modules/home/views/widgets/drawer_builder_view.dart';
+import 'package:news_app/modules/home/views/widgets/selected_category_view.dart';
+import 'package:provider/provider.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+import '../view_model/home_view_model.dart';
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
-  final List<CategoryModel> _categories = [
-    CategoryModel(id: 'general', name: 'General', image: Assets.images.general.path),
-    CategoryModel(id: 'business', name: 'Business', image: Assets.images.busniess.path),
-    CategoryModel(id: 'sports', name: 'Sports', image: Assets.images.sport.path),
-    CategoryModel(id: 'technology', name: 'Technology', image: Assets.images.technology.path),
-    CategoryModel(id: 'entertainment', name: 'Entertainment', image: Assets.images.entertainment.path),
-    CategoryModel(id: 'health', name: 'Health', image: Assets.images.helth.path),
-    CategoryModel(id: 'science', name: 'Science', image: Assets.images.science.path),
-  ];
-
-  CategoryModel? _selectCategory;
+class HomePage extends StatelessWidget {
+ const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
 
     final local = AppLocalizations.of(context)!;
     final theme = Theme.of(context);
+
+    final vm = Provider.of<HomeViewModel>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text(
-          _selectCategory == null ? local.home
-           : _selectCategory!.name ,
+          vm.selectedCategory == null ? local.home
+           : vm.selectedCategory!.name ,
         ),),
         actions: [
           Padding(
@@ -51,15 +38,13 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: AppColors.mainText,
         child: DrawerBuilderView(
           goToHome: (){
-            setState(() {
-              _selectCategory = null;
-            });
+            vm.changeSelectedCategory(null);
             navigatorKey.currentState!.pop();
           },
         ),
       ),
 
-      body: _selectCategory == null ? Padding(
+      body: vm.selectedCategory == null ? Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
         child: Column(
           spacing: 16,
@@ -75,11 +60,10 @@ class _HomePageState extends State<HomePage> {
                   itemBuilder: (context, index) {
                     return GestureDetector(
                       onTap: () {
-                        _selectCategory = _categories[index];
-                        setState(() {});
+                        vm.changeSelectedCategory(vm.categories[index]);
                       },
                       child: CategoryCardItem(
-                        categoryModel: _categories[index],
+                        categoryModel: vm.categories[index],
                         index: index,
                       ),
                     );
@@ -87,15 +71,13 @@ class _HomePageState extends State<HomePage> {
                   separatorBuilder: (context, index) {
                     return SizedBox(height: 16,);
                   },
-                  itemCount: _categories.length,
+                  itemCount: vm.categories.length,
               ),
             )
           ],
         ),
       )
-          : SelectedCategoryView(
-        selectedCategoryModel: _selectCategory!,
-      ),
+          : SelectedCategoryView(),
     );
   }
 }
